9867295d2fe20cfd369d3909902779a84be81496
Bruno Roberto Gomes <bruno@brgomes.com>
2019-12-13 11:32:37 -0300

    Iniciando layout de perguntas

--- resources/views/bonus/index.blade.php
+++ resources/views/bonus/index.blade.php
@@ -4,7 +4,7 @@
 
 	<div class="row">
 		<div class="col-sm-12">
-			<div class="wrapper">
+			<div class="wrapper mb-4">
 				<div class="wrapper-title bg-dark">
 					<div class="row">
 						<div class="col-sm-11">
@@ -21,7 +21,6 @@
 
 									<div class="dropdown-menu" aria-labelledby="dropdown1">
 										<a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalNovaPergunta">{{ __('content.nova-pergunta') }}</a>
-										<a class="dropdown-item" href="#" data-url="{{ route('bonus.nova-pergunta', $liga->id) }}">{{ __('content.nova-pergunta') }}</a>
 									</div>
 								</div>
 							@endif
@@ -32,10 +31,53 @@
 				@if ($perguntas->count() == 0)
 					<div class="alert alert-info">{{ __('message.nenhuma-pergunta-bonus') }}</div>
 				@else
+					Colocar alguma coisa aqui
 				@endif
 			</div>
-	  	</div>
+		</div>
 	</div>
+
+	@if ($perguntas->count() > 0)
+		<div class="row">
+			@foreach ($perguntas as $pergunta)
+				<div class="col-sm-6">
+					<div class="wrapper mb-4">
+						<div class="wrapper-title bg-dark">
+							<div class="row">
+								<div class="col-sm-11">
+									<h2>{{ $pergunta->pergunta }}</h2>
+								</div>
+								<div class="col-sm-1">
+									@if ($jogador->admin)
+										<div class="dropdown">
+											<a class="btn btn-dark dropdown-toggle" href="#" role="button" id="dropdown1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
+
+											<div class="dropdown-menu" aria-labelledby="dropdown1">
+												<a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalNovaPergunta">{{ __('content.editar-pergunta') }}</a>
+												<a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalNovaPergunta">{{ __('content.adicionar-opcao') }}</a>
+												<div class="dropdown-divider"></div>
+												<a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalNovaPergunta">{{ __('content.excluir-pergunta') }}</a>
+											</div>
+										</div>
+									@endif
+								</div>
+							</div>
+						</div>
+						<form>
+						    <div class="form-group col-md-3">
+						      	<label for="inputEmail4">{{ $pergunta->pergunta }}</label>
+						      	<input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+						    </div>
+						    <div class="form-group col-md-3">
+						      	<label for="inputPassword4"></label>
+						      	<input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+						    </div>
+						</form>
+					</div>
+				</div>
+			@endforeach
+		@endif
+  	</div>
 
 	<div class="modal fade" id="ajaxModal"></div>
 
